6c34c184bfff26c8e279b4ada5729208676ef65b
Mert Temiz <merttemizjj@gmail.com>
2021-05-09 18:00:45 +0300

    modified delete button

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -45,11 +45,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     '${DateFormat.yMMMd().format(transactions[index].date)}',
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          label: Text("Delete"),
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
               /* Card design below is an alternative way to use instead of ListTile. */
